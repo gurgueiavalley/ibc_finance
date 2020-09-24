a4428c6605495f22c2b148973ba76a17174ed439
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-24 00:10:36 -0300

    Criação da classe Empresa

--- ibc_financeiro/models.py
+++ ibc_financeiro/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
-#Teste
-class Membro(models.Model):
-    nome = models.CharField(max_length=50)
+class Empresa(models.Model):
+    CNPJ = models.CharField(unique = True, max_length = 18, validators = [MinLengthValidator(14)])
+    nome = models.CharField(max_length = 70)
+    endereco = models.CharField(max_length = 80) 
+    cidade = models.CharField(max_length = 60, default = 'Corrente')
+    descricao = models.CharField(max_length = 100, blank = True, null = True)
+
+    class Meta:
+        db_table = 'empresa'
+
+    def __str__(self):
+        return self.nome
