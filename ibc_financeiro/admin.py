2c856e44e922b4b401de539abebd939199caf05a
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-24 16:55:03 -0300

    Registro do AdminCongregacao

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from ibc_financeiro.models import *
 
+admin.site.register(AdminCongregacao)
 admin.site.register(Empresa)
 admin.site.register(EntradaMissao)
 admin.site.register(Missao)
