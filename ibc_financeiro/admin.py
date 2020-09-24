58bcce66424bc1539378962d4f7369e98654b4be
Caike Da Silva Reis <45696077+caikesilva@users.noreply.github.com>
2020-09-24 20:39:51 -0300

    Adicionando models
    Adicionando models

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from ibc_financeiro.models import *
+from django.contrib.auth.models import Group, User
 
 admin.site.register(AdminCongregacao)
 admin.site.register(Contador)
@@ -8,3 +9,13 @@ admin.site.register(EntradaMissao)
 admin.site.register(Missao)
 admin.site.register(Saida)
 admin.site.register(SaidaCategoria)
+
+admin.site.site_header="IBC FINANCEIRO"
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(Membro)
+admin.site.register(CategoriaEntrada)
+admin.site.register(FormaPagamento)
+admin.site.register(Congregacao)
+admin.site.register(EntradaFinanceiraAvulca)
+admin.site.register(EntradaFinanceira)
--- ibc_financeiro/models.py
+++ ibc_financeiro/models.py
@@ -106,3 +106,57 @@ class SaidaCategoria(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Membro(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=14, unique = True)
+    salario = models.FloatField()
+    telefone = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.nome
+
+class CategoriaEntrada(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nome
+
+class FormaPagamento(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nome
+
+class Congregacao(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.nome
+
+class EntradaFinanceiraAvulca(models.Model):
+    descricao = models.CharField(max_length=200)
+    data = models.DateTimeField(auto_now=True)
+    valor = models.FloatField()
+    congregacao = models.OneToOneField(Congregacao, on_delete=models.CASCADE)
+    admin_congregacao = models.ForeignKey(AdminCongregacao, on_delete=models.CASCADE)   
+    
+    def __str__(self):
+        return self.descricao 
+
+class EntradaFinanceira(models.Model):
+    descricao = models.CharField(max_length=200)
+    data = models.DateTimeField(auto_now=True)
+    valor = models.FloatField()
+    membro = models.ForeignKey(Membro, on_delete=models.CASCADE)
+    congregacao = models.ForeignKey(Congregacao, on_delete=models.CASCADE)
+    categoria_entrada = models.ForeignKey(CategoriaEntrada, on_delete=models.CASCADE)
+    adm_congregacao = models.ForeignKey(AdminCongregacao, on_delete=models.CASCADE)
+    forma_pagamento = models.ForeignKey(FormaPagamento, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.descricao
