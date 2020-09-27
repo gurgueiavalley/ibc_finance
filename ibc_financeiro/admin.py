6146d67af86284e59e0296366ccc7fabfcb1c873
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-27 10:04:39 -0300

    Registro e personalização da classe Contador

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from ibc_financeiro.models import *
 
-class AdministradorAdmin(admin.ModelAdmin):
+class SimplesAdmin(admin.ModelAdmin):
+    actions = None
+    list_display = 'nome', 'descricao',
+    ordering = 'nome',
+    search_fields = 'nome',
+
+class UsuarioAdmin(admin.ModelAdmin):
     actions = None
     list_display = 'username', 'nome', 'email',
     ordering = 'nome',
@@ -10,18 +16,13 @@ class AdministradorAdmin(admin.ModelAdmin):
 
     fields = ('username', 'nome'), ('email', 'senha')
 
-class CategCongAdmin(admin.ModelAdmin):
-    actions = None
-    list_display = 'nome', 'descricao',
-    ordering = 'nome',
-    search_fields = 'nome',
-
 admin.site.site_header = 'IBC Financeiro'
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
 
-admin.site.register(Administrador, AdministradorAdmin)
-admin.site.register(CategoriaEntrada, CategCongAdmin)
-admin.site.register(CategoriaSaida, CategCongAdmin)
-admin.site.register(Congregacao, CategCongAdmin)
+admin.site.register(Administrador, UsuarioAdmin)
+admin.site.register(CategoriaEntrada, SimplesAdmin)
+admin.site.register(CategoriaSaida, SimplesAdmin)
+admin.site.register(Congregacao, SimplesAdmin)
+admin.site.register(Contador, UsuarioAdmin)
