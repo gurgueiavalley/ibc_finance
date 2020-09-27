dfa341e6086fa9253f5f34d998d9f38081d38b27
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-27 01:57:58 -0300

    Registro e personalização da classe Administrador

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from ibc_financeiro.models import *
+
+class AdministradorAdmin(admin.ModelAdmin):
+    actions = None
+    list_display = 'username', 'nome', 'email',
+    ordering = 'nome',
+    search_fields = 'username', 'nome', 'email',
+
+    fields = ('username', 'nome'), ('email', 'senha')
 
 admin.site.site_header = 'IBC Financeiro'
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
+
+admin.site.register(Administrador, AdministradorAdmin)
