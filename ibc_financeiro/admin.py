a07534ad83656dc5f364a3ecb876316f8d99ba8a
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-27 01:32:25 -0300

    Remoção do registro do Usuario

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
 
 admin.site.site_header = 'IBC Financeiro'
 
 admin.site.unregister(Group)
+admin.site.unregister(User)
