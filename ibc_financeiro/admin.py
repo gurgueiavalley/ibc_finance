6a1ebf66631298813722b6baabafc548f6ed4bab
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-27 01:28:42 -0300

    Alteração do nome do cabeçalho

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -1,21 +1,3 @@
 from django.contrib import admin
-from ibc_financeiro.models import *
-from django.contrib.auth.models import Group, User
 
-admin.site.register(AdminCongregacao)
-admin.site.register(Contador)
-admin.site.register(Empresa)
-admin.site.register(EntradaMissao)
-admin.site.register(Missao)
-admin.site.register(Saida)
-admin.site.register(SaidaCategoria)
-
-admin.site.site_header="IBC FINANCEIRO"
-admin.site.unregister(User)
-admin.site.unregister(Group)
-admin.site.register(Membro)
-admin.site.register(CategoriaEntrada)
-admin.site.register(FormaPagamento)
-admin.site.register(Congregacao)
-admin.site.register(EntradaFinanceiraAvulca)
-admin.site.register(EntradaFinanceira)
+admin.site.site_header = 'IBC Financeiro'
