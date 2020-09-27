6a9ba62734eb1b9a7d0a2b6ff96b20820d464568
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-27 11:47:31 -0300

    Registro e personalização da classe Empresa

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from ibc_financeiro.models import *
+import requests
+
+class EmpresaAdmin(admin.ModelAdmin):
+    actions = None
+    list_display = 'CNPJ', 'nome', 'descricao',
+    list_display_links = 'CNPJ', 'nome',
+    list_filter = 'cidade',
+    ordering = 'nome',
+    search_fields = 'CNPJ', 'nome',
+
+    fields = 'CNPJ',
+
+    def save_model(self, request, obj, form, change):
+        CNPJ = obj.CNPJ.replace('.', '').replace('/', '').replace('-', '')
+        webservice = requests.get('http://receitaws.com.br/v1/cnpj/{}'.format(CNPJ))
+        dados = webservice.json()
+        
+        if dados['fantasia'] != '':
+            obj.nome = dados['fantasia']
+
+        else:
+            obj.nome = dados['nome']
+
+        obj.CNPJ = dados['cnpj']
+        obj.descricao = dados['atividade_principal'][0]['text']
+        obj.endereco = dados['logradouro']
+        obj.cidade = dados['municipio']
+
+        super(EmpresaAdmin, self).save_model(request, obj, form, change)
 
 class SimplesAdmin(admin.ModelAdmin):
     actions = None
@@ -26,3 +55,4 @@ admin.site.register(CategoriaEntrada, SimplesAdmin)
 admin.site.register(CategoriaSaida, SimplesAdmin)
 admin.site.register(Congregacao, SimplesAdmin)
 admin.site.register(Contador, UsuarioAdmin)
+admin.site.register(Empresa, EmpresaAdmin)
