636866ccce7e697cdf9945497ad2aab821cc0b0e
KIQRibeiro <caiquewinchester78@gmail.com>
2020-09-27 15:50:33 -0300

    Personalização da classe Entrada

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -38,7 +38,12 @@ class EmpresaAdmin(admin.ModelAdmin):
         super(EmpresaAdmin, self).save_model(request, obj, form, change)
 
 class EntradaAdmin(admin.ModelAdmin):
-    pass
+    actions = None
+    list_display = 'valor', 'categoria', 'membro', 'forma_de_Pagamento', 'data', 'administrador'
+    list_filter = 'categoria', 'data', 'forma_de_Pagamento__nome', 'congregacao__nome', 'congregacao__localidade', 'administrador__nome'
+    search_fields = 'membro__nome', 'valor'
+
+    fields = ('categoria', 'valor', 'forma_de_Pagamento'), 'descricao', ('membro', 'congregacao'), 'data', 'comprovante', 'administrador'
 
 class EntradaAvulsaAdmin(admin.ModelAdmin):
     actions = None
@@ -96,7 +101,7 @@ admin.site.register(CategoriaSaida, SimplesAdmin)
 admin.site.register(Congregacao, CongregacaoAdmin)
 admin.site.register(Contador, UsuarioAdmin)
 admin.site.register(Empresa, EmpresaAdmin)
-admin.site.register(Entrada, EntradaAdmin)              # PERSONALIZAR
+admin.site.register(Entrada, EntradaAdmin)
 admin.site.register(EntradaAvulsa, EntradaAvulsaAdmin)
 admin.site.register(EntradaMissao, EntradaMissaoAdmin)
 admin.site.register(Membro, MembroAdmin)
