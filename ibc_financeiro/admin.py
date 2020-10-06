133c7798ca44b148818b7f346b18f4d0fd34f091
KIQRibeiro <caiquewinchester78@gmail.com>
2020-10-05 22:11:22 -0300

    Remoção de import sem uso

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from ibc_financeiro.models import *
-import requests
 
 class CongregacaoAdmin(admin.ModelAdmin):
     actions = None
