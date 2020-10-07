627dfd7cd8666963a8d387c7a37c47396b890492
Caike Da Silva Reis <45696077+caikesilva@users.noreply.github.com>
2020-10-07 11:56:37 -0300

    funcao excel

--- ibc_financeiro/views.py
+++ ibc_financeiro/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+import pandas as pd
+from . models import Membro
+from decimal import Decimal
 
-# Create your views here.
+# Create your views here
+
+def cadMembrosExcel(request):
+    #Instalar o pandas
+    #pip install pandas
+    dados = pd.read_excel(r"C:\Users\Caike Silva\Desktop\membros.xlsx")
+    membros = []
+    membros = dados
+    for m in range(0, membros.__len__()):
+        membrosExistente = Membro.objects.filter(CPF=membros['CPF'][m])
+        if membrosExistente.__len__() == 1:
+            print("Já existe")
+        else:
+            membro = Membro()
+            membro.CPF = membros['CPF'][m]
+            membro.nome = membros['NOME'][m]
+            membro.telefone = membros['TELEFONE'][m]
+            membro.save()
+    return render(request, 'index.html', {})
+    
+
