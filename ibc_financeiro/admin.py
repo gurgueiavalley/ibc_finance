8a07fabe1ee3689159006c5ffca414263b4b1dde
KIQRibeiro <caiquewinchester78@gmail.com>
2020-10-09 23:15:29 -0300

    Alteração do nome da classe para CategoriaAdmin

--- ibc_financeiro/admin.py
+++ ibc_financeiro/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from ibc_financeiro.models import *
 
+class CategoriaAdmin(admin.ModelAdmin):
+    actions = None
+    list_display = 'nome', 'descricao',
+    ordering = 'nome',
+    search_fields = 'nome',
+
 class CongregacaoAdmin(admin.ModelAdmin):
     actions = None
     list_display = 'nome', 'localidade',
@@ -65,12 +71,6 @@ class SaidaAdmin(admin.ModelAdmin):
 
     fields = 'categoria', ('nome', 'descricao'), 'data', ('valor', 'forma_de_Pagamento', 'empresa'), ('comprovante', 'nota_Fiscal'), 'administrador'
 
-class SimplesAdmin(admin.ModelAdmin):
-    actions = None
-    list_display = 'nome', 'descricao',
-    ordering = 'nome',
-    search_fields = 'nome',
-
 class UsuarioAdmin(admin.ModelAdmin):
     actions = None
     list_display = 'username', 'nome', 'email',
@@ -85,8 +85,8 @@ admin.site.unregister(Group)
 admin.site.unregister(User)
 
 admin.site.register(Administrador, UsuarioAdmin)
-admin.site.register(CategoriaEntrada, SimplesAdmin)
-admin.site.register(CategoriaSaida, SimplesAdmin)
+admin.site.register(CategoriaEntrada, CategoriaAdmin)
+admin.site.register(CategoriaSaida, CategoriaAdmin)
 admin.site.register(Congregacao, CongregacaoAdmin)
 admin.site.register(Contador, UsuarioAdmin)
 admin.site.register(Empresa, EmpresaAdmin)
