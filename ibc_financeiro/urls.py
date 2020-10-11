1e933660772fa91ad1c9a900dec2f26c863ef772
KIQRibeiro <caiquewinchester78@gmail.com>
2020-10-11 18:40:47 -0300

    Organização da ordem

--- ibc_financeiro/urls.py
+++ ibc_financeiro/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadMembrosExcel', views.cadMembrosExcel, name='cadMembrosExcel'),
     path('', views.index, name = 'index'),
+    path('cadMembrosExcel', views.cadMembrosExcel, name='cadMembrosExcel'),
 ]
