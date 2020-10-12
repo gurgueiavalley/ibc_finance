46d71dcbfa172b4466317b6d3a896d581dae28ea
Caike Da Silva Reis <45696077+caikesilva@users.noreply.github.com>
2020-10-11 22:44:06 -0300

    adicionando função relatorio de entrada

--- ibc_financeiro/urls.py
+++ ibc_financeiro/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('relatorio', views.relatorio, name='relatorio'),
     path('cadMembrosExcel', views.cadMembrosExcel, name='cadMembrosExcel'),
     path('', views.index, name = 'index'),
 ]
--- ibc_financeiro/views.py
+++ ibc_financeiro/views.py
@@ -4,9 +4,13 @@ from . models import Membro
 from decimal import Decimal
 from ibc_financeiro.forms import FormExcel
 import os
-from ibc_financeiro.models import Excel
+from ibc_financeiro.models import Entrada, Excel
+from reportlab.pdfgen import canvas
+from django.conf import settings
+from pathlib import Path
 
 # Create your views here
+
 
 def cadMembrosExcel(request):
     #Instalar o pandas
@@ -36,6 +40,34 @@ def cadMembrosExcel(request):
     else:
         form = FormExcel()
     return render(request, 'financeiro/form_excel.html', {'form': form})
+
+def relatorio(request):
+    if request.method == 'POST':
+        print(request.POST)
+        valor_Final = 0;
+        y = 0
+        caminho = "ibc_financeiro/static/relatorio_entrada.pdf"
+        pdf = canvas.Canvas(caminho)
+        pdf.setTitle("Relatório de Entradas")
+        pdf.drawImage('ibc_financeiro/static/images/logo.png', 10,780,height=50, width=60)
+        pdf.setFont('Times-Bold', 24)
+        pdf.drawString(200,800,"Relatório de Entradas")
+        pdf.setFont('Helvetica', 12)
+
+        for entrada in Entrada.objects.filter(data__range=[request.POST['dataInicio'], request.POST['dataFim']]).order_by('data'):
+            y = y + 20
+            if(y > 500):
+                y = 0
+                pdf.showPage()
+            pdf.drawString(100, 750 - y, "Data: " +  str(entrada.data))
+            pdf.drawString(420,750 - y,"Valor: " + str(entrada.valor))
+            valor_Final = valor_Final + entrada.valor
+        pdf.drawString(420,720 - y,"Valor Total: "+str(valor_Final))
+        pdf.save()
+        nome = 'relatorio_entrada.pdf'
+        return render(request, 'index.html', {'nome': nome})
+    else:
+        return render(request, 'financeiro/form_relatorio.html', {})
 
 def index(request):
     return render(request, 'financeiro/index.html')
