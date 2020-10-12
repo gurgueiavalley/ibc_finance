42a2407f5fb80410b7e5183b3f4de13e4f1802a6
KIQRibeiro <caiquewinchester78@gmail.com>
2020-10-12 02:16:13 -0300

    Criação do método relatorioSaida

--- ibc_financeiro/views.py
+++ ibc_financeiro/views.py
@@ -7,8 +7,63 @@ from ibc_financeiro.forms import FormExcel
 import os
 from ibc_financeiro.models import Excel
 
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+
 def index(request):
     return render(request, 'financeiro/index.html')
+
+def relatorioSaida(request):
+    # Criando arquivo
+    PDF = canvas.Canvas('relatorioSaida.pdf')
+    PDF.setTitle('Relatório de Saída')
+
+    # Desenhando coordenadas
+    desenharCoordenadas(PDF)
+
+    # Título
+    PDF.setFont('Times-Bold', 16)
+    PDF.drawCentredString(300, 770, 'Relatório de Saída')
+    
+    # Subtítulo
+    PDF.setFillColorRGB(0, 0, 255)
+    PDF.setFont('Times-Roman', 12)
+    PDF.drawCentredString(290, 720, 'Relatório de Saída 2')
+
+    # Linha
+    PDF.line(30, 710, 550, 710)
+
+    # Texto
+    texto = PDF.beginText(40, 680)
+    texto.setFont('Courier', 12)
+    texto.setFillColor(colors.orange)
+    texto.textLine('Primeira linha')
+    texto.textLine('Segunda linha')
+    PDF.drawText(texto)
+
+    # Imagem
+    PDF.drawInlineImage('ibc_financeiro/static/images/logo.png', 130, 400, height = 40, width = 40)
+
+    # Salvando
+    PDF.save()
+    
+    return render(request, 'financeiro/index.html')
+
+def desenharCoordenadas(PDF):
+    PDF.drawString(100, 810, 'x100')
+    PDF.drawString(200, 810, 'x200')
+    PDF.drawString(300, 810, 'x300')
+    PDF.drawString(400, 810, 'x400')
+    PDF.drawString(500, 810, 'x500')
+
+    PDF.drawString(10, 100, 'y100')
+    PDF.drawString(10, 200, 'y200')
+    PDF.drawString(10, 300, 'y300')
+    PDF.drawString(10, 400, 'y400')
+    PDF.drawString(10, 500, 'y500')
+    PDF.drawString(10, 600, 'y600')
+    PDF.drawString(10, 700, 'y700')
+    PDF.drawString(10, 800, 'y800')
 
 def cadMembrosExcel(request):
     #Instalar o pandas
