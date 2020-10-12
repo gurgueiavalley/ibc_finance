b01c1289ee3d934dc5a58e17ca109015f74e7e54
KIQRibeiro <caiquewinchester78@gmail.com>
2020-10-12 20:14:46 -0300

    Organizando paths

--- ibc_financeiro/urls.py
+++ ibc_financeiro/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('relatorio/<tipo>', views.relatorio, name = 'relatorio'),
-    
     path('cadMembrosExcel', views.cadMembrosExcel, name='cadMembrosExcel'),
 ]
