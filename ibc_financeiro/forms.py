43d73e4c107eab9364c67cd727631cb1a3932735
KIQRibeiro <caiquewinchester78@gmail.com>
2020-10-14 00:20:33 -0300

    Adição da classe SaidaForm

--- ibc_financeiro/forms.py
+++ ibc_financeiro/forms.py
@@ -1,4 +1,13 @@
 from django import forms
+from . import models
 
 class FormExcel(forms.Form):
     arquivo = forms.FileField()
+
+class SaidaForm(forms.Form):
+    categorias = []
+
+    for category in CategoriaSaida.objects.all():
+        categorias.append(category.nome)
+
+    categoria = forms.ChoiceField(choices = [categorias], required= False)
