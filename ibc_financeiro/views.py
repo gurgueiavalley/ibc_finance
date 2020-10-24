7c977c19574bf153e4f1b9542921496181fbcac7
KIQRibeiro <caiquewinchester78@gmail.com>
2020-10-23 21:40:45 -0300

    Reposição dos métodos excluídos

--- ibc_financeiro/views.py
+++ ibc_financeiro/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
+
+from .models import *
+from .forms import *
+
 import pandas as pd
-from . models import Membro
 from decimal import Decimal
-from ibc_financeiro.forms import FormExcel
 import os
-from ibc_financeiro.models import Congregacao, Entrada, EntradaAvulsa, EntradaMissao, Excel
+from datetime import datetime
+
 from reportlab.pdfgen import canvas
 from django.conf import settings
 from pathlib import Path
-import datetime
-from reportlab.lib import colors
-from reportlab.platypus import SimpleDocTemplate
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import Table
-from reportlab.platypus import TableStyle
 
 def index(request):
     return render(request, 'financeiro/index.html')
@@ -57,7 +54,7 @@ def relatorio(request):
         caminho = "ibc_financeiro/static/relatorio_entrada.pdf"
         pdf = canvas.Canvas(caminho)
         pdf.setTitle("Relatório de Entradas")
-        pdf.drawImage('ibc_financeiro/static/images/logo.png', 10,780,height=50, width=60)
+        pdf.drawImage('ibc_financeiro/static/imagens/logo.png', 10,780,height=50, width=60)
         pdf.setFont('Times-Bold', 24)
         pdf.drawString(200,800,"Relatório de Entradas")
         pdf.setFont('Times-Bold', 16)
@@ -117,105 +114,32 @@ def relatorio(request):
         congregacao = Congregacao.objects.all()
         return render(request, 'financeiro/form_relatorio.html', {'congregacao': congregacao})
 
-def relatorioSaida(request):
-    return render(request, 'financeiro/paginas/relatorio.html')
+def relatorio(request, tipo):
+    if tipo == 'saida':
+        if request.method == 'POST':
+            print(listaSaida(request))
+        
+        return render(request, 'financeiro/paginas/relatorios/saida.html', {'formulario' : RelatorioSaidaForm()})
 
+# Métodos Auxiliares
+def convertDate(date):
+    return datetime.strptime(date, '%d/%m/%Y').date()
 
+def listaSaida(request):
+    datas = [convertDate(request.POST['inicio']), convertDate(request.POST['fim'])]
 
-def relatorioSaida1(request):
-    # Criando arquivo
-    PDF = canvas.Canvas('relatorioSaida.pdf')
-    PDF.setTitle('Relatório de Saída')
+    congregacoes = request.POST.getlist('congregacao')
+    categorias = request.POST.getlist('categoria')
+    pagamentos = request.POST.getlist('pagamento')
+    empresas = request.POST.getlist('empresa')
+    valores = [request.POST['minimo'], request.POST['maximo']]
 
-    # Desenhando coordenadas
-    desenharCoordenadas(PDF)
+    saidas = Saida.objects.filter(data__range = datas).order_by('data')
 
-    # Título
-    PDF.setFont('Times-Bold', 16)
-    PDF.drawCentredString(300, 770, 'Relatório de Saída')
-    
-    # Subtítulo
-    PDF.setFillColorRGB(0, 0, 255)
-    PDF.setFont('Times-Roman', 12)
-    PDF.drawCentredString(290, 720, 'Relatório de Saída 2')
+    saidas = saidas.filter(congregacao__nome__in = congregacoes) if congregacoes != [] else saidas
+    saidas = saidas.filter(categoria__nome__in = categorias) if categorias != [] else saidas
+    saidas = saidas.filter(forma_de_Pagamento__nome__in = pagamentos) if pagamentos != [] else saidas
+    saidas = saidas.filter(empresa__nome__in = empresas) if empresas != [] else saidas
+    saidas = saidas.filter(valor__in = valores) if valores != ['', ''] else saidas
 
-    # Linha
-    PDF.line(30, 710, 550, 710)
-
-    # Texto
-    texto = PDF.beginText(40, 680)
-    texto.setFont('Courier', 12)
-    texto.setFillColor(colors.orange)
-    texto.textLine('Primeira linha')
-    texto.textLine('Segunda linha')
-    PDF.drawText(texto)
-
-    # Imagem
-    PDF.drawInlineImage('ibc_financeiro/static/images/logo.png', 130, 400, height = 40, width = 40)
-
-    # Salvando
-    PDF.save()
-    
-    return render(request, 'financeiro/index.html')
-
-def desenharCoordenadas(PDF):
-    PDF.drawString(100, 810, 'x100')
-    PDF.drawString(200, 810, 'x200')
-    PDF.drawString(300, 810, 'x300')
-    PDF.drawString(400, 810, 'x400')
-    PDF.drawString(500, 810, 'x500')
-
-    PDF.drawString(10, 100, 'y100')
-    PDF.drawString(10, 200, 'y200')
-    PDF.drawString(10, 300, 'y300')
-    PDF.drawString(10, 400, 'y400')
-    PDF.drawString(10, 500, 'y500')
-    PDF.drawString(10, 600, 'y600')
-    PDF.drawString(10, 700, 'y700')
-    PDF.drawString(10, 800, 'y800')
-
-def relatorioSaida2(request):
-    # Dados
-    dados = [
-        ['Nome', 'Valor'],
-        ['Monitor', 'R$800,00'],
-        ['Cadeira', 'R$320,00'],
-        ['SSD', 'R$260,00'],
-    ]
-    
-    PDF = SimpleDocTemplate(
-        'relatorioSaida.pdf',
-        pagesize = letter
-    )
-
-    tabela = Table(dados)
-    
-    style = TableStyle([
-        ('BACKGROUND', (0, 0), (3, 0), colors.blue),
-        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
-
-        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-
-        ('FONTNAME', (0, 0), (-1, 0), 'Times-Bold'),
-        ('FONTSIZE', (0, 0), (-1, 0), 13),
-
-        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-
-        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
-
-        ('BOX', (0, 0), (-1, -1), 2, colors.black),
-
-        ('LINEBEFORE', (1, 1), (2, -1), 2, colors.red),
-        ('LINEABOVE', (0, 2), (-1, 2), 2, colors.green),
-
-        ('GRID', (0, 1), (-1, -1), 2, colors.black),
-    ])
-    tabela.setStyle(style)
-
-    elementos = []
-    elementos.append(tabela)
-
-    PDF.build(elementos)
-
-    return render(request, 'financeiro/index.html')
- 
+    return saidas 
