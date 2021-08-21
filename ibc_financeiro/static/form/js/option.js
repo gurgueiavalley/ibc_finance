3f12818d458f3c40ab21349443ebd023edeced84
KIQRibeiro <caiquewinchester78@gmail.com>
2021-08-21 19:46:02 -0300

    Renomeando o método create para insert

--- ibc_financeiro/static/form/js/option.js
+++ ibc_financeiro/static/form/js/option.js
@@ -10,7 +10,7 @@ const Option = {
         }))
     },
 
-    create : function(className, name, id){
+    insert : function(className, name, id){
         this.div = this.div(`#id_${className.toLowerCase()}`).parent()
         this.text = name
         this.value = id
